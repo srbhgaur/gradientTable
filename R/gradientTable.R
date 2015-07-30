fe39a8d7ddd382861c11c12335dab08d367b3464
Saurabh Gaur <sr7@saurabhgaur.com>
2015-07-30 16:41:49 +0530

    Option to choose cols working. Do not pass non-numeric, non-factor cols. Color options are hardcoded.

--- R/gradientTable.R
+++ R/gradientTable.R
@@ -1,15 +1,22 @@
-#' <Add Title>
+#' Create gradient tables
 #'
-#' <Add Description>
+#' gradientTable(iris)
 #'
 #' @import htmlwidgets
 #'
 #' @export
-gradientTable <- function(message, width = NULL, height = NULL) {
+gradientTable <- function(indata, colstoColor=c(), width = NULL, height = NULL) {
+  cols = colnames(indata)
+  if(length(colstoColor) < 1){ colstoColor=cols } # if unspecified,color all columns
+  #Add Colors
+  for (x in colstoColor) {
+        indata[paste0(x,".Color")] <- genGradient(indata[x][[1]])
+  }
 
   # forward options using x
   x = list(
-    message = message
+    indata = jsonlite::toJSON(indata),
+    cols = cols
   )
 
   # create widget
@@ -35,4 +42,26 @@ gradientTableOutput <- function(outputId, width = '100%', height = '400px'){
 renderGradientTable <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
   shinyRenderWidget(expr, gradientTableOutput, env, quoted = TRUE)
+}
+
+
+#' custom html function for gradientTable
+#' @import htmltools
+gradientTable_html <- function(id, style, class, ...){
+  tagList(
+    tags$div( id = id, class = class, style = style
+              ,tags$div(class = "chart-main",id="intable")
+    )
+  )
+}
+
+genGradient<-function(x,type="gradient",palette="Blues"){
+  if (is.factor(x)) {
+    n=length(levels(x))
+    labels <- RColorBrewer::brewer.pal(n, "Accent")
+    cut(ecdf(x)(x),c(0:n)/n,labels=labels)
+  } else {
+    labels <- c("#FFFFFF", RColorBrewer::brewer.pal(9, palette))
+    cut(ecdf(x)(x),c(0:10)/10,labels=labels)
+  }
 }
--- inst/htmlwidgets/gradientTable.js
+++ inst/htmlwidgets/gradientTable.js
@@ -13,8 +13,43 @@ HTMLWidgets.widget({
   },
 
   renderValue: function(el, x, instance) {
+    function tabulate(data, columns) {
+            var table = d3.select("#tabledata"),
+                thead = table.append("thead"),
+                tbody = table.append("tbody");
 
-    el.innerText = x.message;
+            // append the header row
+            thead.append("tr")
+                .selectAll("th")
+                .data(columns)
+                .enter()
+                .append("th")
+                    .text(function(column) { return column; });
+
+            // create a row for each object in the data
+            var rows = tbody.selectAll("tr")
+                .data(data)
+                .enter()
+                .append("tr");
+
+            // create a cell in each row for each column
+            var cells = rows.selectAll("td")
+                .data(function(row) {
+                    return columns.map(function(column) {
+                        return {column: column, value: row[column],color : row[column+".Color"]};
+                    });
+                })
+                .enter()
+                .append("td")
+                    .text(function(d) { return d.value; })
+                    .style("background-color",function(d) { return d.color||"lightgrey"; });
+
+            return table;
+        }
+
+    d3.select('#intable').html( '<table cellpadding="2px" cellspacing="0" border="0" class="display" id="tabledata"></table>' );
+
+    var intable = tabulate(x.indata,x.cols);
 
   },
 
